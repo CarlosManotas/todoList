45d0ff0837a6e45bb05912806dce7567db02ce00
CarlosManotas <carlosmanotas@hotmail.com>
2018-06-26 11:02:23 -0300

    api initdata

--- src/api.js
+++ src/api.js
@@ -1,18 +1,15 @@
-export const getItems = () => {
+const initData = () => {
   const listOfItems = window.localStorage.getItem('listOfItems')
-  return new Promise((resolve) => {
-    resolve(JSON.parse(listOfItems) || [])
-  })
+  return JSON.parse(listOfItems) || []
 }
+export const getItems = () => new Promise(resolve => resolve(initData()))
 export const addItem = item => {
-  const listOfItems = window.localStorage.getItem('listOfItems')
-  const newList = JSON.parse(listOfItems) || []
+  const newList = initData()
   window.localStorage.setItem('listOfItems', JSON.stringify([...newList, item]))
   return new Promise(resolve => setTimeout(() => resolve([...newList, item]), 1000))
 }
 export const removeItem = item => {
-  const listOfItems = window.localStorage.getItem('listOfItems')
-  const newList = JSON.parse(listOfItems)
+  const newList = initData()
   const deletedValue = [...newList].filter(itemList => itemList !== item)
   window.localStorage.setItem('listOfItems', JSON.stringify(deletedValue))
   return new Promise(resolve => setTimeout(() => resolve(deletedValue), 1000))
